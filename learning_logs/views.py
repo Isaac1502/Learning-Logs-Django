6ca0f255606ef43234a5db507110c197cb714921
Isaac Tadina Giraldo <itadina@growthaccelerationpartners.com>
2023-12-18 17:33:55 -0500

    edit entries added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -8,11 +8,13 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
+
 
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
@@ -24,6 +26,7 @@ def topic(request, topic_id):
     }
 
     return render(request, 'learning_logs/topic.html', context)
+
 
 def new_topic(request):
     """Add a new topic."""
@@ -40,6 +43,7 @@ def new_topic(request):
     # Display a blank or invalid form
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+
 
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
@@ -60,3 +64,24 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
